5d00e9d12038913b2bd2f5605e8fa0936dc34fb8
Maria Flor Di Piero <mariaflordipiero@gmail.com>
2023-01-23 21:24:17 -0300

    aula 1

---
+++ type-string.js
@@ -0,0 +1,34 @@
+const texto1 = "Olá Mundo";
+const texto2 = 'Olá Mundo';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "123456";
+
+const citacao = 'O Leo disse "oi"';
+const meuNome = "Leonardo";
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha2 = "minhaSenha123";
+console.log(senha.length) // 13 caracteres
+
+
+
+// concatenação (+)
+console.log(citacao + meuNome)
+
+// template string ou template literal
---
+++ type.number.js
@@ -0,0 +1,26 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 2;
+
+const segundoNumero = 3;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN Not a Number (Não é um Número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
