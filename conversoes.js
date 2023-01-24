a85255ff66af3a8df37414d3cfdb9787f778f79c
Maria Flor Di Piero <mariaflordipiero@gmail.com>
2023-01-24 19:45:23 -0300

    Aula 2

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleanos
+
+//conversão implícita
+
+const numero = 456;
+const numeroString = "456"
+
+console.log(numero === numeroString) //false
+console.log(numero == numeroString) //true
+
+console.log(numero + numeroString)
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
